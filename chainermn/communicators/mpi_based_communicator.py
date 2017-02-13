5619c1f1863cbdb1ac8f09cde49b331342a7a74a
Takuya Akiba <t.akiba.65536@gmail.com>
2017-02-13 16:20:24 +0900

    Add scatter_dataset

--- chainermn/communicators/mpi_based_communicator.py
+++ chainermn/communicators/mpi_based_communicator.py
@@ -1,3 +1,6 @@
+import chainer.datasets
+
+
 class MPIBasedCommunicator(object):
 
     def __init__(self, mpi_comm):
@@ -16,3 +19,21 @@ class MPIBasedCommunicator(object):
     @property
     def size(self):
         return self.mpi_comm.size
+
+    def scatter_dataset(self, dataset):
+        # TODO(akiba): write why we do not use mpi_comm.scatter
+
+        if self.rank == 0:
+            mine = None
+            n_samples = len(dataset)
+            for i in range(self.size):
+                b = n_samples * i // self.size
+                e = n_samples * (i + 1) // self.size
+                subds = chainer.datasets.SubDataset(dataset=dataset, start=b, finish=e)
+                if i == 0:
+                    mine = subds
+                else:
+                    self.mpi_comm.send(subds, dest=i)
+            return mine
+        else:
+            return self.mpi_comm.recv(source=0)
