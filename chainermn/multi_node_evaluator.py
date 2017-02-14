7e7e13e51beb0cecf599caae378e17b03ba3c733
Takuya Akiba <t.akiba.65536@gmail.com>
2017-02-14 17:00:41 +0900

    Add MultiNodeEvaluator

---
+++ chainermn/multi_node_evaluator.py
@@ -0,0 +1,20 @@
+import chainer.training.extensions
+import mpi4py.MPI
+
+
+class MultiNodeEvaluator(chainer.training.extensions.Evaluator):
+
+    def __init__(self, comm, *args, **kwargs):
+        super(MultiNodeEvaluator, self).__init__(*args, **kwargs)
+
+        self.comm = comm
+        if hasattr(self.comm, 'mpi_comm'):
+            self.comm = self.comm.mpi_comm
+
+    def evaluate(self):
+        local_mean_dict = super(MultiNodeEvaluator, self).evaluate()
+        global_mean_dict = {
+            name: self.comm.allreduce(value, op=mpi4py.MPI.SUM) / self.comm.size
+            for name, value in sorted(local_mean_dict.items())
+        }
+        return global_mean_dict
