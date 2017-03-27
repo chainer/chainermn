6566f464a5af938b7ba9256842c9cc1f46f6691b
Takuya Akiba <t.akiba.65536@gmail.com>
2017-03-27 18:25:02 +0900

    Change the interface to create multi node optimizer to follow that of evaluator

--- chainermn/__init__.py
+++ chainermn/__init__.py
@@ -2,5 +2,5 @@ from chainermn.communicators import get_communicator  # NOQA
 from chainermn.dataset import get_epoch_trigger  # NOQA
 from chainermn.dataset import get_n_iterations_for_one_epoch  # NOQA
 from chainermn.dataset import scatter_dataset  # NOQA
-from chainermn.multi_node_evaluator import MultiNodeEvaluator  # NOQA
-from chainermn.multi_node_optimizer import MultiNodeOptimizer  # NOQA
+from chainermn.multi_node_evaluator import create_multi_node_evaluator  # NOQA
+from chainermn.multi_node_optimizer import create_multi_node_optimizer  # NOQA
