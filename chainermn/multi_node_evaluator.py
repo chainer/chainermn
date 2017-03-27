b2fa27142dcd6f3b57e2191d19ca53ff83772c92
Takuya Akiba <t.akiba.65536@gmail.com>
2017-03-27 14:28:58 +0900

    Enhance MultiNodeEvaluator to support Python 2

--- chainermn/multi_node_evaluator.py
+++ chainermn/multi_node_evaluator.py
@@ -1,3 +1,6 @@
+import six
+
+
 class MultiNodeEvaluator(object):
 
     def __init__(self, actual_evaluator, communicator):
@@ -8,6 +11,11 @@ class MultiNodeEvaluator(object):
             'communicator', communicator)
         super(MultiNodeEvaluator, self).__setattr__(
             'actual_evaluator', actual_evaluator)
+
+        if six.PY2:
+            class T(MultiNodeEvaluator, type(actual_evaluator)):
+                pass
+            self._t = T
 
     def evaluate(self):
         local_mean_dict = self.actual_evaluator.evaluate()
@@ -21,8 +29,30 @@ class MultiNodeEvaluator(object):
         return getattr(self.actual_evaluator, attr_name)
 
     def __setattr__(self, attr_name, value):
-        setattr(self.actual_evaluator, attr_name, value)
+        if attr_name == '__class__':
+            super(MultiNodeEvaluator, self).__setattr__(attr_name, value)
+        else:
+            setattr(self.actual_evaluator, attr_name, value)
 
     def __call__(self, *args, **kwargs):
         # TODO(akiba): explain that __call__ is not resolved by __getattr__
-        type(self.actual_evaluator).__call__(self, *args, **kwargs)
+        # TODO(akiba): explain that we need to deceive type check of Python 2
+        # Problem 1: __call__ unavalable for objects with different types
+        #   http://ideone.com/d9S5Gr -- Python 3, standard way works
+        #   http://ideone.com/dMXJTS -- Python 2, failure by type check
+        #   http://ideone.com/FD7VIv -- Python 2, hack
+        # Problem 2: methods of wrapper objects are not called
+        #   http://ideone.com/6OCgMb -- Python 3, desired behaviour
+        #   http://ideone.com/MobQmY -- Python 2, undesired behaviour
+        #   http://ideone.com/e8eI4M -- Python 2, more dirty hack
+
+        if six.PY3:
+            type(self.actual_evaluator).__call__(self, *args, **kwargs)
+        else:
+            t = type(self.actual_evaluator)
+            c = self.__class__
+            try:
+                self.__class__ = self._t
+                t.__call__(self, *args, **kwargs)
+            finally:
+                self.__class__ = c
