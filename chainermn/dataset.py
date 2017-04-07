fe1f41296103ccef215016d68003cfddcec144f4
Takuya Akiba <t.akiba.65536@gmail.com>
2017-04-07 18:21:50 +0900

    Add docstrings for dataset utility functions

--- chainermn/dataset.py
+++ chainermn/dataset.py
@@ -2,13 +2,29 @@ import chainer.datasets
 
 
 def scatter_dataset(dataset, comm):
-    # TODO(akiba): write why we do not use mpi_comm.scatter
+    """Scatter the given dataset to the workers in the communicator.
+
+    The dataset of worker 0 (i.e., the worker whose ``comm.rank`` is 0) is
+    scattered to all workers. The given dataset of other workers are ignored.
+    The dataset is split to sub datasets of almost equal sizes and scattered
+    to workers. To create a sub dataset, ``chainer.datasets.SubDataset`` is
+    used.
+
+    Args:
+        dataset: A dataset (e.g., ``list``, ``numpy.ndarray``,
+            ``chainer.datasets.TupleDataset``, ...).
+        comm: ChainerMN communicator or MPI4py communicator.
+
+    Returns:
+        Scattered dataset.
+    """
 
     if hasattr(comm, 'mpi_comm'):
         comm = comm.mpi_comm
     assert hasattr(comm, 'send')
     assert hasattr(comm, 'recv')
 
+    # TODO(akiba): write why we do not use mpi_comm.scatter
     if comm.rank == 0:
         mine = None
         n_samples = len(dataset)
@@ -26,6 +42,16 @@ def scatter_dataset(dataset, comm):
 
 
 def get_n_iterations_for_one_epoch(dataset, local_batch_size, comm):
+    """Get the number of iterations for one epoch.
+
+    Args:
+        dataset: Sub dataset of each worker.
+        local_batch_size (int): Batch size of each worker.
+        comm: ChainerMN communicator or MPI4py communicator.
+
+    Returns:
+        int: the number of iterations for one epoch.
+    """
     if hasattr(comm, 'mpi_comm'):
         comm = comm.mpi_comm
     assert hasattr(comm, 'bcast')
@@ -38,6 +64,17 @@ def get_n_iterations_for_one_epoch(dataset, local_batch_size, comm):
 
 
 def get_epoch_trigger(n_epochs, dataset, local_batch_size, comm):
+    """Get the trigger that behaves like an epoch trigger.
+
+    Args:
+        n_epochs (int): The number of epochs.
+        dataset: Sub dataset of each worker.
+        local_batch_size (int): Batch size of each worker.
+        comm: ChainerMN communicator or MPI4py communicator.
+
+    Returns:
+        The trigger that behaves like the epoch trigger.
+    """
     n_iterations = n_epochs * get_n_iterations_for_one_epoch(
         dataset, local_batch_size, comm)
     return n_iterations, 'iteration'
