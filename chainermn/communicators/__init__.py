ddbbbf88550b55e65e3233ed16524d2676698065
Takuya Akiba <t.akiba.65536@gmail.com>
2017-04-11 17:48:24 +0900

    Add explanation and comparison of communicators to docstring of create_communicator

--- chainermn/communicators/__init__.py
+++ chainermn/communicators/__init__.py
@@ -4,12 +4,32 @@ import mpi4py.MPI
 def create_communicator(communicator_name='hierarchical', mpi_comm=mpi4py.MPI.COMM_WORLD):
     """Create a ChainerMN communicator.
 
+    Different communicators provide different approaches of communication, so
+    they have different performance charasteristics. The default communicator
+    ``hierarchical`` is expected to generally perform well on a variety of
+    environments, so one need not to change communicators in most cases.
+    However, choosing proper communicator may give better performance.
+    The following communicators are available.
+
+    =============== === === ======== =======================================
+    Name            CPU GPU NCCL     Recommended Use Cases
+    =============== === === ======== =======================================
+    naive           OK  OK           Testing on CPU mode
+    flat                OK           Each node has one GPU
+    hierarchical        OK  Required Each node has multiple GPUs and one HCA
+    two_dimensional     OK  Required Each node has multiple GPUs and HCAs
+    single_node         OK  Required Single node with multiple GPUs
+    =============== === === ======== =======================================
+
     Args:
-        communicator_name: The name of communicator (``naive``, ``hierarchical``, ``two_dimensional``, or ``single_node``)
+        communicator_name: The name of communicator (``naive``, ``flat``,
+          ``hierarchical``, ``two_dimensional``, or ``single_node``)
         mpi_comm: MPI4py communicator
 
     Returns:
         ChainerMN communicator
+
+
     """
     if communicator_name == 'naive':
         from chainermn.communicators.naive_communicator \
