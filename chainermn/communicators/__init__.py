7d5e7f897ca65671787fe173204c493bdcfc5b08
Takuya Akiba <t.akiba.65536@gmail.com>
2017-04-11 14:17:42 +0900

    Simplify arguments of create_communicator method

--- chainermn/communicators/__init__.py
+++ chainermn/communicators/__init__.py
@@ -1,33 +1,36 @@
-def create_communicator(communicator_name='hierarchical', *args, **kwargs):
+import mpi4py.MPI
+
+
+def create_communicator(communicator_name='hierarchical', mpi_comm=mpi4py.MPI.COMM_WORLD):
     if communicator_name == 'naive':
         from chainermn.communicators.naive_communicator \
             import NaiveCommunicator
-        return NaiveCommunicator(*args, **kwargs)
+        return NaiveCommunicator(mpi_comm=mpi_comm)
 
     elif communicator_name == 'flat':
         from chainermn.communicators.flat_communicator \
             import FlatCommunicator
-        return FlatCommunicator(*args, **kwargs)
+        return FlatCommunicator(mpi_comm=mpi_comm)
 
     elif communicator_name == 'hierarchical':
         from chainermn.communicators.hierarchical_communicator \
             import HierarchicalCommunicator
-        return HierarchicalCommunicator(*args, **kwargs)
+        return HierarchicalCommunicator(mpi_comm=mpi_comm)
 
     elif communicator_name == 'two_dimensional':
         from chainermn.communicators.two_dimensional_communicator \
             import TwoDimensionalCommunicator
-        return TwoDimensionalCommunicator(*args, **kwargs)
+        return TwoDimensionalCommunicator(mpi_comm=mpi_comm)
 
     elif communicator_name == 'single_node':
         from chainermn.communicators.single_node_communicator \
             import SingleNodeCommunicator
-        return SingleNodeCommunicator(*args, **kwargs)
+        return SingleNodeCommunicator(mpi_comm=mpi_comm)
 
     elif communicator_name == 'dummy':
         from chainermn.communicators.dummy_communicator \
             import DummyCommunicator
-        return DummyCommunicator(*args, **kwargs)
+        return DummyCommunicator(mpi_comm=mpi_comm)
 
     else:
         raise ValueError(
