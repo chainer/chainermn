7918132bb14a2e481a7dfef810ca734df2a03d23
Keisuke Fukuda <kfukuda@preferred.jp>
2017-04-13 14:36:33 +0900

    try a bugfix on travis

--- .travis_build_mpi.sh
+++ .travis_build_mpi.sh
@@ -6,7 +6,7 @@ BUILD_DIR=$HOME/mpi-build
 mkdir -p ~/mpi
 mkdir -p $BUILD_DIR
 
-function install {
+function install_mpich {
     PREFIX=$HOME/mpi/$1
     DIST=$2
     FILE=$(basename $DIST)
@@ -25,11 +25,20 @@ function install {
         curl $DIST >$FILE
         tar -xf $FILE
         cd $(find . -maxdepth 1 -mindepth 1 -type d)
-        ./configure --prefix=$PREFIX && make -j4 && make install
+        ./configure --prefix=$PREFIX \
+                    --disable-f77 \
+                    --disable-fc \
+                    --enable-cxx \
+                    --enable-shared=yes
+        make -j4 && make install
     fi
+
+    $PREFIX/mpicxx -show
 }
 
-install mpich-3.0.4 "http://www.mpich.org/static/downloads/3.0.4/mpich-3.0.4.tar.gz"
+install_mpich mpich-3.0.4 \
+              "http://www.mpich.org/static/downloads/3.0.4/mpich-3.0.4.tar.gz"
+
 # install mpich32 "http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz"
 # install ompi110 "https://www.open-mpi.org/software/ompi/v1.10/downloads/openmpi-1.10.6.tar.bz2"
 # install ompi108 "https://www.open-mpi.org/software/ompi/v1.8/downloads/openmpi-1.8.8.tar.bz2"
