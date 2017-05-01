8b58f46ef9e2c09dbb1daeacdd27e0f3837d2c7a
Takuya Akiba <t.akiba.65536@gmail.com>
2017-05-01 22:44:21 +0900

    Simplify the error message generation

--- chainermn/communicators/_base.py
+++ chainermn/communicators/_base.py
@@ -6,12 +6,11 @@ class NodeAwareCommunicatorBase(object):
 
     def __init__(self, mpi_comm, use_nccl):
         if use_nccl and not nccl._available:
-            msg = (
+            raise RuntimeError(
                 'NCCL is not available. '
                 'Please confirm that NCCL can be found by dynamic linkers, '
                 'and ChainerMN is installed without --no-nccl flag.'
             )
-            raise RuntimeError(msg)
 
         self.mpi_comm = mpi_comm
         self.use_nccl = use_nccl
