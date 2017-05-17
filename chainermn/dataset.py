00d1669d3cc6e361495f67c0f0f6d339699d4dc6
Takuya Akiba <t.akiba.65536@gmail.com>
2017-05-17 11:02:40 +0900

    Restore trigger utility functions and label them as deprecated

--- chainermn/dataset.py
+++ chainermn/dataset.py
@@ -1,4 +1,5 @@
 import chainer.datasets
+import warnings
 
 
 def scatter_dataset(dataset, comm):
@@ -40,3 +41,60 @@ def scatter_dataset(dataset, comm):
         return mine
     else:
         return comm.recv(source=0)
+
+
+def get_n_iterations_for_one_epoch(dataset, local_batch_size, comm):
+    """Get the number of iterations for one epoch.
+
+    .. note::
+
+        This API is deprecated. Please use standard epoch triggers.
+
+    Args:
+        dataset: Sub dataset of each worker.
+        local_batch_size (int): Batch size of each worker.
+        comm: ChainerMN communicator or MPI4py communicator.
+
+    Returns:
+        int: the number of iterations for one epoch.
+    """
+
+    warnings.warn(
+        'get_n_iterations_for_one_epoch is deprecated. Please use '
+        'standard epoch triggers.', DeprecationWarning)
+
+    if hasattr(comm, 'mpi_comm'):
+        comm = comm.mpi_comm
+    assert hasattr(comm, 'bcast')
+
+    n_iterations = None
+    if comm.rank == 0:
+        n_iterations = (len(dataset) + local_batch_size -
+                        1) // local_batch_size
+    return comm.bcast(n_iterations)
+
+
+def get_epoch_trigger(n_epochs, dataset, local_batch_size, comm):
+    """Get the trigger that behaves like an epoch trigger.
+
+    .. note::
+
+        This API is deprecated. Please use standard epoch triggers.
+
+    Args:
+        n_epochs (int): The number of epochs.
+        dataset: Sub dataset of each worker.
+        local_batch_size (int): Batch size of each worker.
+        comm: ChainerMN communicator or MPI4py communicator.
+
+    Returns:
+        The trigger that behaves like the epoch trigger.
+    """
+
+    warnings.warn(
+        'get_epoch_trigger is deprecated. Please use standard epoch triggers.',
+        DeprecationWarning)
+
+    n_iterations = n_epochs * get_n_iterations_for_one_epoch(
+        dataset, local_batch_size, comm)
+    return n_iterations, 'iteration'
