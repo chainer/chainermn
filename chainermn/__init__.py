4376d87eaec4d2af26f5c96ac1c2c51d36f5fb90
Takuya Akiba <t.akiba.65536@gmail.com>
2017-05-17 11:05:57 +0900

    Add __version__

--- chainermn/__init__.py
+++ chainermn/__init__.py
@@ -1,6 +1,11 @@
+import pkg_resources
+
 from chainermn.communicators import create_communicator  # NOQA
 from chainermn.dataset import get_epoch_trigger  # NOQA
 from chainermn.dataset import get_n_iterations_for_one_epoch  # NOQA
 from chainermn.dataset import scatter_dataset  # NOQA
 from chainermn.multi_node_evaluator import create_multi_node_evaluator  # NOQA
 from chainermn.multi_node_optimizer import create_multi_node_optimizer  # NOQA
+
+
+__version__ = pkg_resources.get_distribution('chainermn').version
