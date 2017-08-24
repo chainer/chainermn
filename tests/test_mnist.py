21951ed17679abb0b71c2c8c38287d72efddaeb0
Yuichiro Ueno <y1r.ueno@gmail.com>
2017-08-24 17:11:21 +0900

    Add MNIST test on GPU

--- tests/test_mnist.py
+++ tests/test_mnist.py
@@ -6,13 +6,15 @@ import unittest
 import chainer
 import chainer.functions as F
 import chainer.links as L
+import chainer.testing
+import chainer.testing.attr
 from chainer import training
 from chainer.training import extensions
+
 import chainermn
 
 
 class MLP(chainer.Chain):
-
     def __init__(self, n_units, n_out):
         super(MLP, self).__init__(
             l1=L.Linear(784, n_units),
@@ -26,16 +28,28 @@ class MLP(chainer.Chain):
         return self.l3(h2)
 
 
+@chainer.testing.parameterize(
+    {'gpu': True},
+    {'gpu': False},
+)
 class TestMNIST(unittest.TestCase):
     def test_mnist(self, display_log=True):
-        # This test file is intended to be run on Travis-CI and
-        # GPU is not used for now.
         epoch = 5
         batchsize = 100
         n_units = 100
 
-        comm = chainermn.create_communicator('naive')
+        if self.gpu:
+            comm = chainermn.create_communicator('hierarchical')
+            device = comm.intra_rank
+            chainer.cuda.get_device(device).use()
+        else:
+            comm = chainermn.create_communicator('naive')
+            device = -1
+
         model = L.Classifier(MLP(n_units, 10))
+        if self.gpu:
+            model.to_gpu()
+
         optimizer = chainermn.create_multi_node_optimizer(
             chainer.optimizers.Adam(), comm)
         optimizer.setup(model)
@@ -53,11 +67,11 @@ class TestMNIST(unittest.TestCase):
                                                      repeat=False,
                                                      shuffle=False)
 
-        updater = training.StandardUpdater(train_iter, optimizer)
+        updater = training.StandardUpdater(train_iter, optimizer, device=device)
         trainer = training.Trainer(updater, (epoch, 'epoch'))
 
         # Wrap standard Chainer evaluators by MultiNodeEvaluator.
-        evaluator = extensions.Evaluator(test_iter, model)
+        evaluator = extensions.Evaluator(test_iter, model, device=device)
         evaluator = chainermn.create_multi_node_evaluator(evaluator, comm)
         trainer.extend(evaluator)
 
