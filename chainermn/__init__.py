a4d91a2561d76a1ed55be9353a1fe93adf939c80
Takuya Akiba <t.akiba.65536@gmail.com>
2017-08-28 17:29:58 +0900

    Move scatter_dataset to datasets module

--- chainermn/__init__.py
+++ chainermn/__init__.py
@@ -1,6 +1,7 @@
 import pkg_resources
+
 from chainermn.communicators import create_communicator  # NOQA
-from chainermn.dataset import scatter_dataset  # NOQA
+from chainermn.datasets import scatter_dataset  # NOQA
 from chainermn.extensions import create_multi_node_evaluator  # NOQA
 from chainermn.links import MultiNodeChainList  # NOQA
 from chainermn.multi_node_optimizer import create_multi_node_optimizer  # NOQA
