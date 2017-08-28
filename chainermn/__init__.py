77eaffc0f18fc513f07b8496235d5212c77f2185
Takuya Akiba <t.akiba.65536@gmail.com>
2017-08-28 17:07:15 +0900

    Move MultiNodeEvaluator to extensions module

--- chainermn/__init__.py
+++ chainermn/__init__.py
@@ -1,9 +1,8 @@
 import pkg_resources
-
 from chainermn.communicators import create_communicator  # NOQA
 from chainermn.dataset import scatter_dataset  # NOQA
+from chainermn.extensions import create_multi_node_evaluator  # NOQA
 from chainermn.link import MultiNodeChainList  # NOQA
-from chainermn.multi_node_evaluator import create_multi_node_evaluator  # NOQA
 from chainermn.multi_node_optimizer import create_multi_node_optimizer  # NOQA
 
 
