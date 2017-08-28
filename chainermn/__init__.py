b60cb60fc39f767f1ca194d28628e06e4b80b92b
Takuya Akiba <t.akiba.65536@gmail.com>
2017-08-28 17:31:08 +0900

    Move MultiNodeOptimizer to optimizers module

--- chainermn/__init__.py
+++ chainermn/__init__.py
@@ -4,7 +4,7 @@ from chainermn.communicators import create_communicator  # NOQA
 from chainermn.datasets import scatter_dataset  # NOQA
 from chainermn.extensions import create_multi_node_evaluator  # NOQA
 from chainermn.links import MultiNodeChainList  # NOQA
-from chainermn.multi_node_optimizer import create_multi_node_optimizer  # NOQA
+from chainermn.optimizers import create_multi_node_optimizer  # NOQA
 
 
 __version__ = pkg_resources.get_distribution('chainermn').version
