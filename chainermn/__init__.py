ae2a006f88d668430a0ec3dc4b905346eb8bec9e
Takuya Akiba <t.akiba.65536@gmail.com>
2017-08-28 17:19:10 +0900

    Move MultiNodeChainList to links module

--- chainermn/__init__.py
+++ chainermn/__init__.py
@@ -2,7 +2,7 @@ import pkg_resources
 from chainermn.communicators import create_communicator  # NOQA
 from chainermn.dataset import scatter_dataset  # NOQA
 from chainermn.extensions import create_multi_node_evaluator  # NOQA
-from chainermn.link import MultiNodeChainList  # NOQA
+from chainermn.links import MultiNodeChainList  # NOQA
 from chainermn.multi_node_optimizer import create_multi_node_optimizer  # NOQA
 
 
