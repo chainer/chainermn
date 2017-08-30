f2ee572a7d31fc8478e557f529060559e65457ae
Keisuke Fukuda <keisukefukuda@gmail.com>
2017-08-30 14:24:12 +0900

    removed redundant import

--- chainermn/__init__.py
+++ chainermn/__init__.py
@@ -3,7 +3,6 @@ import pkg_resources
 from chainermn.communicators import create_communicator  # NOQA
 from chainermn.dataset import DataSizeError  # NOQA
 from chainermn.dataset import scatter_dataset  # NOQA
-from chainermn.dataset import DataSizeError  # NOQA
 from chainermn.link import MultiNodeChainList  # NOQA
 from chainermn.multi_node_evaluator import create_multi_node_evaluator  # NOQA
 from chainermn.multi_node_optimizer import create_multi_node_optimizer  # NOQA
