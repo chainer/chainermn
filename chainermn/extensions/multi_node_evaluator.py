7122a9777088f06042fe62987789c73145f688f1
UENISHI Kota <kota@preferred.jp>
2017-12-14 16:53:22 +0900

    Clean up

--- chainermn/extensions/multi_node_evaluator.py
+++ chainermn/extensions/multi_node_evaluator.py
@@ -1,23 +1,26 @@
 import types
 
 
-def create_multi_node_evaluator(actual_evaluator, communicator):
+def create_multi_node_evaluator(evaluator, communicator):
     """Create a multi node evaluator from a normal evaluator.
 
-    Actually patches the evaluator to work with multinode
-    optimizer.
+    Actually patches the evaluator to work in multinode
+    environment. This method adds several hidden attributes starting
+    with _mn_ prefix. After the patch, original evaluator does not
+    work correctly in non-MPI environment.
 
     Args:
-        actual_evaluator: evaluator
+        evaluator: evaluator to be patched
             (e.g., ``chainer.training.extensions.Evaluator``)
         communicator: ChainerMN communicator
 
     Returns:
-        The multi node evaluator based on ``actual_evaluator``.
+        The multi node evaluator based on ``evaluator``.
+
     """
 
-    actual_evaluator._mn_original_evaluate = actual_evaluator.evaluate
-    actual_evaluator._mn_communicator = communicator
+    evaluator._mn_original_evaluate = evaluator.evaluate
+    evaluator._mn_communicator = communicator
 
     def new_evaluate(self):
         local_mean_dict = self._mn_original_evaluate()
@@ -29,6 +32,5 @@ def create_multi_node_evaluator(actual_evaluator, communicator):
         }
         return global_mean_dict
 
-    actual_evaluator.evaluate = types.MethodType(
-        new_evaluate, actual_evaluator)
-    return actual_evaluator
+    evaluator.evaluate = types.MethodType(new_evaluate, evaluator)
+    return evaluator
