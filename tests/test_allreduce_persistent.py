c46244fed4aae17929520e9baff81725173dd064
Takuya Akiba <t.akiba.65536@gmail.com>
2017-12-18 19:14:12 +0900

    Add tests for AllreducePeresistent

---
+++ tests/test_allreduce_persistent.py
@@ -0,0 +1,51 @@
+import chainer
+import chainer.testing
+import chainer.testing.attr
+import unittest
+
+import chainermn
+
+
+class ExampleModel(chainer.Chain):
+    def __init__(self, n_in=3, n_units=5, n_out=2):
+        super(ExampleModel, self).__init__(
+            l1=chainer.links.Linear(n_in, n_units, nobias=True),
+            bn1=chainer.links.BatchNormalization(n_units),
+            l2=chainer.links.Linear(n_units, n_units, nobias=True),
+            bn2=chainer.links.BatchNormalization(n_units),
+            l3=chainer.links.Linear(n_units, n_out),
+        )
+
+
+class TestAllreducePersistent(unittest.TestCase):
+
+    def setUp(self):
+        self.comm = chainermn.create_communicator('naive')
+
+    def _test(self, model):
+        xp = model.xp
+        rank = self.comm.rank
+
+        model.bn1.avg_mean += xp.full_like(model.bn1.avg_mean, rank * 1)
+        model.bn2.avg_mean += xp.full_like(model.bn2.avg_mean, rank * 2)
+        model.bn1.avg_var += xp.full_like(model.bn1.avg_var, rank * 3)
+        model.bn2.avg_var += xp.full_like(model.bn2.avg_var, rank * 4)
+
+        allreduce_persistent = \
+            chainermn.extensions.AllreducePersistent(model, self.comm)
+        allreduce_persistent()
+
+        avg_rank = (self.comm.size - 1) / 2
+        chainer.testing.assert_allclose(model.bn1.avg_mean, avg_rank * 1)
+        chainer.testing.assert_allclose(model.bn2.avg_mean, avg_rank * 2)
+        chainer.testing.assert_allclose(model.bn1.avg_var, avg_rank * 3)
+        chainer.testing.assert_allclose(model.bn2.avg_var, avg_rank * 4)
+
+    def test_allreduce_persistent_cpu(self):
+        self._test(ExampleModel())
+
+    @chainer.testing.attr.gpu
+    def test_allreduce_persistent_gpu(self):
+        model = ExampleModel()
+        model.to_gpu()
+        self._test(model)
