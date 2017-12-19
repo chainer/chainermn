0074eb6904d72635a9b308036e070b1ca95a9f02
Takuya Akiba <t.akiba.65536@gmail.com>
2017-12-19 12:11:14 +0900

    Fix communicator initialization for CPU mode

--- tests/test_allreduce_persistent.py
+++ tests/test_allreduce_persistent.py
@@ -19,35 +19,33 @@ class ExampleModel(chainer.Chain):
 
 class TestAllreducePersistent(unittest.TestCase):
 
-    def setUp(self):
-        self.comm = chainermn.create_communicator('hierarchical')
-        device = self.comm.intra_rank
-        chainer.cuda.get_device(device).use()
-
-    def _test(self, model):
-        xp = model.xp
-        rank = self.comm.rank
-
+    def _test(self, comm, model):
+        rank = comm.rank
         model.bn1.avg_mean.fill(rank * 1)
         model.bn2.avg_mean.fill(rank * 2)
         model.bn1.avg_var.fill(rank * 3)
         model.bn2.avg_var.fill(rank * 4)
 
         allreduce_persistent = \
-            chainermn.extensions.AllreducePersistent(model, self.comm)
+            chainermn.extensions.AllreducePersistent(model, comm)
         allreduce_persistent()
 
-        avg_rank = (self.comm.size - 1) / 2
+        avg_rank = (comm.size - 1) / 2
         chainer.testing.assert_allclose(model.bn1.avg_mean, avg_rank * 1)
         chainer.testing.assert_allclose(model.bn2.avg_mean, avg_rank * 2)
         chainer.testing.assert_allclose(model.bn1.avg_var, avg_rank * 3)
         chainer.testing.assert_allclose(model.bn2.avg_var, avg_rank * 4)
 
     def test_allreduce_persistent_cpu(self):
-        self._test(ExampleModel())
+        comm = chainermn.create_communicator('naive')
+        self._test(comm, ExampleModel())
 
     @chainer.testing.attr.gpu
     def test_allreduce_persistent_gpu(self):
+        comm = chainermn.create_communicator('hierarchical')
+        device = comm.intra_rank
+        chainer.cuda.get_device(device).use()
+
         model = ExampleModel()
         model.to_gpu()
-        self._test(model)
+        self._test(comm, model)
