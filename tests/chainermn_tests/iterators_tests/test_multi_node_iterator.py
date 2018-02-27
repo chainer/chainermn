00839ce26c1408771d6ec6a6ccd269bdf340f012
levelfour <levelfourslv@gmail.com>
2018-02-27 16:38:49 +0900

    add a test overwriting batch order for multi node iterator

--- tests/chainermn_tests/iterators_tests/test_multi_node_iterator.py
+++ tests/chainermn_tests/iterators_tests/test_multi_node_iterator.py
@@ -7,6 +7,39 @@ import pytest
 import unittest
 
 
+class DummySerializer(chainer.serializer.Serializer):
+
+    def __init__(self, target):
+        super(DummySerializer, self).__init__()
+        self.target = target
+
+    def __getitem__(self, key):
+        raise NotImplementedError
+
+    def __call__(self, key, value):
+        self.target[key] = value
+        return self.target[key]
+
+
+class DummyDeserializer(chainer.serializer.Deserializer):
+
+    def __init__(self, target):
+        super(DummyDeserializer, self).__init__()
+        self.target = target
+
+    def __getitem__(self, key):
+        raise NotImplementedError
+
+    def __call__(self, key, value):
+        if value is None:
+            value = self.target[key]
+        elif isinstance(value, np.ndarray):
+            np.copyto(value, self.target[key])
+        else:
+            value = type(value)(np.asarray(self.target[key]))
+        return value
+
+
 class TestMultiNodeIterator(unittest.TestCase):
 
     def setUp(self):
@@ -99,3 +132,29 @@ class TestMultiNodeIterator(unittest.TestCase):
                         self.communicator.mpi_comm.ssend(batch, dest=0)
             except StopIteration:
                 continue
+
+    def test_overwrite_order(self):
+        """
+        This test confirms that master's batch order can be overwritten,
+        while slave's batch order cannot be overwritten, since slave must
+        always distribute the completely same batch as master.
+        """
+        bs = 4
+        rank_master = 0
+        iterator = chainermn.iterators.create_multi_node_iterator(
+            chainer.iterators.SerialIterator(
+                self.dataset, batch_size=bs, shuffle=True, repeat=False),
+            self.communicator,
+            rank_master=rank_master)
+
+        order = iterator._order
+        new_order = np.roll(order, 1)
+        target = dict()
+        iterator.serialize(DummySerializer(target))
+        target['order'] = new_order
+        iterator.serialize(DummyDeserializer(target))
+
+        if self.communicator.rank == rank_master:
+            self.assertEqual(iterator._order.tolist(), new_order.tolist())
+        else:
+            self.assertEqual(iterator._order.tolist(), order.tolist())
