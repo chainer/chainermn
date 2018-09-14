0024f43ede5431ae3a3ae1df60c9ecb05f7c8344
Shuji Suzuki <ssuzuki@preferred.jp>
2018-09-14 09:49:34 +0900

    refactoring

--- chainermn/functions/batch_normalization.py
+++ chainermn/functions/batch_normalization.py
@@ -80,12 +80,14 @@ class _NcclMultiNodeBatchNormalizationCommunicator(object):
 
     def __init__(self, comm, workspace):
         self.comm = comm
-        self.workspace = workspace
+        self._workspace = workspace
 
         # We need to delay importing MPI4py (and momdules that import MPI4py)
         from chainermn.communicators.pure_nccl_communicator import \
             _get_nccl_type_id
         from chainermn import nccl
+        import chainermn.communicators._memory_utility as memory_utility_module
+        self.memory_utility_module = memory_utility_module
 
         self.nccl = nccl
         self.get_nccl_type_id = _get_nccl_type_id
@@ -94,21 +96,23 @@ class _NcclMultiNodeBatchNormalizationCommunicator(object):
         # ChainerMN diff (1/2) begins
         gpu_buffer_n_elems = gamma.size * 2
         gpu_buffer_size = x.dtype.itemsize * gpu_buffer_n_elems
-        self.workspace.gpu_buffer_a.assign(gpu_buffer_size)
-        self.workspace.gpu_buffer_b.assign(gpu_buffer_size)
-        gpu_buffer_a_array = self.workspace.gpu_buffer_a.array(
+        gpu_buffer_a = self.memory_utility_module.DeviceMemory()
+        gpu_buffer_b = self.memory_utility_module.DeviceMemory()
+        gpu_buffer_a.assign(gpu_buffer_size)
+        gpu_buffer_b.assign(gpu_buffer_size)
+        gpu_buffer_a_array = gpu_buffer_a.array(
             gpu_buffer_n_elems, dtype=x.dtype)
         x.mean(axis=axis, out=gpu_buffer_a_array[:gamma.size])
         xp.square(x).mean(axis=axis, out=gpu_buffer_a_array[gamma.size:])
         stream = chainer.cuda.Stream.null
         self.comm._init_comms()
-        self.comm.nccl_comm.allReduce(self.workspace.gpu_buffer_a.ptr(),
-                                      self.workspace.gpu_buffer_b.ptr(),
+        self.comm.nccl_comm.allReduce(gpu_buffer_a.ptr(),
+                                      gpu_buffer_b.ptr(),
                                       gpu_buffer_n_elems,
                                       self.get_nccl_type_id(x.dtype),
                                       self.nccl.NCCL_SUM,
                                       stream.ptr)
-        gpu_buffer_b_array = self.workspace.gpu_buffer_b.array(
+        gpu_buffer_b_array = gpu_buffer_b.array(
             gpu_buffer_n_elems,
             dtype=x.dtype)
         gpu_buffer_b_array *= 1.0 / self.comm.size
@@ -124,22 +128,22 @@ class _NcclMultiNodeBatchNormalizationCommunicator(object):
         # (instead of multiplying 1/size to gbeta, ggamma)
         gpu_buffer_n_elems = gamma.size * 2
         gpu_buffer_size = x.dtype.itemsize * gpu_buffer_n_elems
-        self.workspace.gpu_buffer_a.assign(gpu_buffer_size)
-        self.workspace.gpu_buffer_b.assign(gpu_buffer_size)
-        gpu_buffer_a_array = self.workspace.gpu_buffer_a.array(
+        self._workspace.gpu_buffer_a.assign(gpu_buffer_size)
+        self._workspace.gpu_buffer_b.assign(gpu_buffer_size)
+        gpu_buffer_a_array = self._workspace.gpu_buffer_a.array(
             gpu_buffer_n_elems,
             dtype=x.dtype)
         gy.sum(axis=axis, out=gpu_buffer_a_array[:gamma.size])
         (gy * x_hat).sum(axis=axis, out=gpu_buffer_a_array[gamma.size:])
         stream = chainer.cuda.Stream.null
         self.comm._init_comms()
-        self.comm.nccl_comm.allReduce(self.workspace.gpu_buffer_a.ptr(),
-                                      self.workspace.gpu_buffer_b.ptr(),
+        self.comm.nccl_comm.allReduce(self._workspace.gpu_buffer_a.ptr(),
+                                      self._workspace.gpu_buffer_b.ptr(),
                                       gpu_buffer_n_elems,
                                       self.get_nccl_type_id(x.dtype),
                                       self.nccl.NCCL_SUM,
                                       stream.ptr)
-        gpu_buffer_b_array = self.workspace.gpu_buffer_b.array(
+        gpu_buffer_b_array = self._workspace.gpu_buffer_b.array(
             gpu_buffer_n_elems,
             dtype=x.dtype)
         gpu_buffer_b_array *= 1.0 / self.comm.size
